d121d5035c201ba57b4526370284c90cddc2c2ba
pila89 <salim.amara@outlook.com>
2023-06-24 12:35:22 +0200

    add loading section

--- test/src/app/components/products/products.component.ts
+++ test/src/app/components/products/products.component.ts
@@ -5,25 +5,26 @@ import { ProductService } from 'src/app/services/product.service';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent {
   products?: Product[];
-  
+  loading = false;
   constructor(private _productsService: ProductService) {}
 
   ngOnInit(): void {
     this.getAllProducts();
   }
   getAllProducts() {
+    this.loading = true;
     this._productsService.getAllProducts().subscribe(
       (response) => {
         this.products = response.products;
+        this.loading = false;
       },
       (error) => {
         console.log(error);
       }
     );
   }
-
 }
